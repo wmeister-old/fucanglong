1615a541abf4668d87dd92efe0a76d92920d2249
William Meister <wmeister86@gmail.com>
2012-02-18 00:03:32 -0600

    added texture and visible fields

---
+++ cache.lua
@@ -0,0 +1,40 @@
+module(...., package.seeall)
+
+local _cache = {}
+
+function is_cached(key)
+   if _cache[key] then
+      return true
+   else
+      return false
+   end
+end
+
+function cache(key, value)
+   if value then
+      _cache[key] = value
+   end
+   return _cache[key]
+end
+
+function res(name)
+   return "res/"..name
+end
+
+function cache_image(name)
+   local key = res(name)
+
+   if is_cached(key) then
+      return cache(key)
+   else
+      return cache(key, love.graphics.newImage(key))
+   end
+end
+
+function cache_png(name)
+   return cache_image(name..".png")
+end
+
+function cache_texture(name)
+   return cache_png("texture/"..name)
+end
--- dialog.lua
+++ dialog.lua
@@ -1,8 +1,17 @@
 module(..., package.seeall)
 
 -- TODO need border color
-new = base_object.constructor("dialog", {"x", "y", "width", "height", "bgcolor?", "color?", "text?"}, {bgcolor = "#000000", color = "#ffffff"})
+new = base_object.constructor("dialog", {"x", "y", "width", "height", "texture?", "border?", "bgcolor?", "color?", "text?", "visible?"},
+                                        {bgcolor = "#000000", color = "#ffffff", visible = true})
 
-
-
-
+function draw(dlg)
+   if dlg.texture then
+      if not cache.is_cached(dlg.texture) then
+	 cache.cache_texture(dlg.texture)
+      end
+      -- TODO draw the texture
+   else
+      -- TODO draw a rectangle filled w/ bgcolor
+   end
+   -- TODO draw the border
+end
