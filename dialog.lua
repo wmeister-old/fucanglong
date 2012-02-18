89a4fc75a2ea521aa7cbe003ca24cdea15f735a4
William Meister <wmeister86@gmail.com>
2012-02-18 08:50:39 -0600

    draw w/ bgcolor implemented in dialog.lua

--- dialog.lua
+++ dialog.lua
@@ -2,9 +2,20 @@ module(..., package.seeall)
 
 -- TODO need border color
 new = base_object.constructor("dialog", {"x", "y", "width", "height", "texture?", "border?", "bgcolor?", "color?", "text?", "visible?"},
-                                        {bgcolor = "#000000", color = "#ffffff", visible = true})
+                                        {bgcolor = colors.BLUE, color = colors.WHITE, visible = true})
 
 function draw(dlg)
+   -- TODO move these into rectangle.lua
+   local function draw_solid_rectangle(color, x, y, width, height) -- TODO rename rectangle.draw_solid
+      love.graphics.setColor(color)
+      love.graphics.rectangle("line", x, y, width, height)
+   end
+
+   local function draw_filled_rectangle(color, x, y, width, height) -- TODO rename rectangle.draw_filled
+      love.graphics.setColor(color)
+      love.graphics.rectangle("fill", x, y, width, height)
+   end
+
    if dlg.texture then
       if not cache.is_cached(dlg.texture) then
 	 cache.cache_texture(dlg.texture)
@@ -12,6 +23,11 @@ function draw(dlg)
       -- TODO draw the texture
    else
       -- TODO draw a rectangle filled w/ bgcolor
+      draw_filled_rectangle(dlg.bgcolor, dlg.x+4, dlg.y+4, dlg.width-8, dlg.height-8)
    end
-   -- TODO draw the border
+
+   draw_solid_rectangle(colors.BLACK, dlg.x, dlg.y, dlg.width, dlg.height)
+   draw_solid_rectangle(colors.DARK_GREY, dlg.x+1, dlg.y+1, dlg.width-2, dlg.height-2)
+   draw_solid_rectangle(colors.LIGHT_GREY, dlg.x+2, dlg.y+2, dlg.width-4, dlg.height-4)
+   draw_solid_rectangle(colors.BLACK, dlg.x+3, dlg.y+3, dlg.width-6, dlg.height-6)
 end
--- main.lua
+++ main.lua
@@ -1,11 +1,28 @@
-dofile("table.lua")
 dofile("dumper.lua")
+dofile("table.lua")
+require("path")
+require("colors")
 require("base_object")
 require("dialog")
 
-local dlg = dialog.new{x = 0,
-		       y = 1,
-		       width = 500,
-		       height = 100}
 
-dump(dlg)
+function love.load()
+   love.graphics.setMode(512, 448)
+   love.graphics.setBackgroundColor(colors.WHITE)
+
+   dlg = dialog.new{x = 0,
+		    y = 1,
+		    width = 500,
+		    height = 100}
+
+   print("dumping dlg:")
+   dump(dlg)
+end
+
+function love.update()
+
+end
+
+function love.draw()
+   dialog.draw(dlg)
+end
